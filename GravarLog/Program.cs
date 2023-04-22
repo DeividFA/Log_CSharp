3c036b8a39670d344902274643b2e5243e389b62
DeividFA <83548426+DeividFA@users.noreply.github.com>
2023-04-21 22:49:28 -0300

    Commit GitHub

---
+++ GravarLog/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GravarLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NomeArquivoLog;
+            string unidade = @"C:\";
+
+
+            NomeArquivoLog = DateTime.Now.ToString();
+            NomeArquivoLog = NomeArquivoLog.Replace('/', '_').Replace(':', '_');
+            NomeArquivoLog = unidade + @"Temp\LOG\Applicacao\Log-" + NomeArquivoLog + ".txt";
+
+            Console.WriteLine("Criando Objeto.");
+            GravarLog log = new GravarLog(NomeArquivoLog);
+
+            Console.WriteLine("Chamando metodo de inserir texto e salvando no arquivo de log.");
+            log.SalvarLog("Teste de Log");
+
+            Console.WriteLine("Chamando metodo de controle para limpar logs antigos");
+            log.DeleteLog(-1); // numero de dias para excluir o arquivo sendo: 0 - nunca excluir, > 0 dias que vao ficar
+
+            Console.WriteLine("Apontando a referencia do objeto pra null para GC retirar da memoria");
+            log = null;
+
+            Console.WriteLine("Programa encerrado tecle enter para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
